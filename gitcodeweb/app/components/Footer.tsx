4e7650fe51336cfcd3dc76a6fe3431b113f4f1cc
akashstwt <akashpatel115347@gmail.com>
2024-12-07 21:09:42 +0530

    footer updated

--- gitcodeweb/app/components/Footer.tsx
+++ gitcodeweb/app/components/Footer.tsx
@@ -1,27 +1,71 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 
-type Props = {}
+type Props = {};
+
+const PlatformCard: React.FC<{ text: string }> = ({ text }) => {
+  return (
+    <div className="flex items-center justify-center w-[150px] h-[50px] text-[#B5F001]">
+      <span className="text-[32px]">{`{`}</span>
+      <span className="text-[16px] mx-1 mt-4">{text}</span>
+      <span className="text-[32px] ">{`}`}</span>
+    </div>
+  );
+};
 
 const Footer = (props: Props) => {
   return (
-    <div className='flex justify-center items-center  text-[#B5F001]'>
-        <div className='lg:w-[1356px] lg:h-[695px] bg-black rounded-lg flex flex-col items-center' >
-            <div className='w-[1136px] h-[298px] flex flex-col justify-between mt-14' >
-                <div className='flex flex-col ' >
-                <span className='text-[160px] text-center font-trap text-[#B5F001]' >We Are-Boost</span>
-                <span className='text-[18px] w-[380px] text-start font-thin '>Grow your impact, and be part of a thriving network where every contribution counts.</span>
-                <div className='flex '>
-                    <input type='text' placeholder=" Your e-mail" className='w-[380px] h-[50px] bg-black text-[#B5F001] text-[18px] font-thin align-middle border-b-2 border-[#B5F001]' /> <Image src='/Arrow1.ico' alt='Arrow icon' width={44} height={2} className='m-4' />
-                </div>
-                </div>
-            <div className='flex flex-row '>
-                <Image src='/sms.svg' alt='sms icon' className='w-[50px] h-[50px] m-4' width={44} height={2}/>
-            </div>
-            </div>
-        </div>
-    </div>
-  )
-}
+    <div className=''>
+      <div className="flex justify-between items-center bg-[#B5F001] mb-6 mx-24  h-28 rounded-lg px-6 shadow-md">
 
-export default Footer
+      <div className="text-black font-medium">
+        <p className="text-[32px]">Stay Updated with</p>
+        <p className="text-[30px] font-bold">GitCode-v3!</p>
+      </div>
+      <div className=''>
+      <button className="flex items-center justify-center bg-black text-white text-[18px] font-medium w-[150px] h-[60px] rounded-full">
+        Join us
+        <div className='bg-[#B5F001] -rotate-45 ml-4 rounded-full'>
+          <span className="p-2 text-[24px] text-black">→</span>
+        </div>
+        
+      </button>
+      </div>
+      </div>
+
+      <div className="flex justify-center items-center text-[#B5F001]">
+      <div className="lg:w-[1356px] lg:h-[695px] bg-black rounded-lg flex flex-col items-center">
+        <div className="w-[1136px] h-[298px] flex flex-col justify-between mt-14">
+          <div className="flex flex-col">
+            <span className="text-[160px] text-center font-trap text-[#B5F001]">We Are-Boost</span>
+            <span className="text-[18px] w-[380px] text-start font-thin">
+              Grow your impact, and be part of a thriving network where every contribution counts.
+            </span>
+            <div className="flex">
+              <input
+                type="text"
+                placeholder="Your e-mail"
+                className="w-[380px] h-[50px] bg-black text-[#B5F001] text-[18px] font-thin align-middle border-b-2 border-[#B5F001]"
+              />
+              <Image src="/Arrow1.ico" alt="Arrow icon" width={44} height={2} className="mt-4 border-b-2 border-[#B5F001] pb-5 " />
+            </div>
+          </div>
+          <div className="flex flex-row w-full items-center justify-between gap-6 mt-32">
+            <div className="flex flex-row items-center gap-2">
+              <Image src="/sms.svg" alt="sms icon" className="w-[50px] h-[50px] m-4" width={44} height={2} />
+              <span>Get in touch</span>
+            </div>
+            <div className='flex'>
+            <PlatformCard text=" x.com " />
+            <PlatformCard text=" Linkedin " />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    
+  );
+};
+
+export default Footer;
