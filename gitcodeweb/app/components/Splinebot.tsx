f039f79f7f46360cbcd4d7ccbafb1e340b4576ea
akashstwt <akashpatel115347@gmail.com>
2024-12-07 16:27:08 +0530

    spline-added

---
+++ gitcodeweb/app/components/Splinebot.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline/next';
+
+const Splinebot = () => {
+  return (
+    <div className='w-[300px] h-[300px]'>
+        <Spline
+        scene="https://prod.spline.design/6zqR6qiVBWt8MU1Y/scene.splinecode" 
+      /> 
+    </div>
+  )
+}
+
+export default Splinebot
