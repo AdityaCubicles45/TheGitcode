906e67fbc6ae39df2244f9bd93e2ddb542dc9a33
AdityaCubicles45 <67513926+AdityaCubicles45@users.noreply.github.com>
2024-12-08 06:45:04 +0530

    minor fix

--- gitcodeweb/app/claim/page.tsx
+++ gitcodeweb/app/claim/page.tsx
@@ -4,13 +4,22 @@ const Claim = () => {
   return (
     <div className="min-h-screen bg-gray-900 flex items-center justify-center p-8 md:p-16">
       <div className="grid md:grid-cols-2 gap-8 md:justify-between items-center h-full w-full">
-        
         {/* Left Section: Claim Bounty Header */}
         <div className="h-full p-8 md:p-16">
           <div className="bg-black text-lime-400 border border-lime-400 h-full w-full md:w-[475px] rounded-2xl relative">
             <h1 className="text-4xl md:text-5xl font-bold mt-6 md:mt-10 ml-6 md:ml-10 leading-snug">
               CLAIM YOUR BOUNTY
             </h1>
+            <p className="text-xl md:text-base mt-4 md:mt-6 px-6 md:px-10 leading-relaxed">
+              Decentralized Donation & Bounty Platform. Revolutionize the
+              funding and sustainability of open-source software projects. Our
+              platform, built on Internet Computer Protocol Blockchain with
+              off-chain computation using Chainlink Functions, enables sponsors
+              to seamlessly support their favorite projects through monthly
+              donations. Enjoy transparent transactions, fair distribution of
+              funds, and automated payout mechanisms, reshaping the landscape of
+              open-source.
+            </p>
             <div className="absolute top-0 right-0 -mt-6 md:-mt-8 -mr-6 md:-mr-8 bg-gray-900 rounded-full w-20 md:w-24 h-20 md:h-24">
               <div className="absolute inset-0 m-auto bg-gray-900 border-4 border-lime-400 rounded-full w-14 md:w-16 h-14 md:h-16"></div>
             </div>
@@ -19,7 +28,9 @@ const Claim = () => {
 
         {/* Right Section: Form */}
         <div className="text-lime-400 p-8 md:p-16">
-          <h2 className="text-2xl md:text-3xl font-semibold mb-4 md:mb-6">let's go !!</h2>
+          <h2 className="text-2xl md:text-3xl font-semibold mb-4 md:mb-6">
+            let's go !!
+          </h2>
           <p className="mb-4 md:mb-6 text-sm">
             Make sure your submitted PR has been merged!
           </p>
